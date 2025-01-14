75e646343a548cc428c2a6cbfb8ec15147d1dc5e
KrukX_rn50@student.itstep.org <KrukX_rn50@student.itstep.org>
2025-01-14 20:43:30 +0200

    .

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  AUTH_KEY: 'amsytech_auth_user',
+  API_URL: 'http://localhost:3001/api', // Оновлено відповідно до вашого сервера
+  VERSION: '1.0.0'
+};
+
+Object.freeze(CONFIG);
+
---
+++ js/localStorage.js
@@ -0,0 +1,34 @@
+const LocalStorage = {
+  setUser: (user) => {
+    const userData = {
+      name: user.name,
+      surname: user.surname,
+      email: user.email,
+      expiration: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString() // Термін дії 7 днів
+    };
+    localStorage.setItem(CONFIG.AUTH_KEY, JSON.stringify(userData));
+  },
+  
+  getUser: () => {
+    const user = localStorage.getItem(CONFIG.AUTH_KEY);
+    if (user) {
+      const userData = JSON.parse(user);
+      if (new Date(userData.expiration) > new Date()) {
+        return userData;
+      } else {
+        LocalStorage.removeUser(); // Видаляємо прострочені дані
+        return null;
+      }
+    }
+    return null;
+  },
+  
+  removeUser: () => {
+    localStorage.removeItem(CONFIG.AUTH_KEY);
+  },
+  
+  isLoggedIn: () => {
+    return LocalStorage.getUser() !== null;
+  }
+};
+
