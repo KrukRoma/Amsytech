58f1a9ce5f3cdb63734f8352bfe134810789ede5
KrukX_rn50@student.itstep.org <KrukX_rn50@student.itstep.org>
2025-01-28 23:28:23 +0200

    ..

--- js/script.js
+++ js/script.js
@@ -3,27 +3,28 @@ window.addEventListener('load', function () {
 
     // Оновлення іконки користувача при завантаженні сторінки
     const userIcon = document.getElementById('user-icon');
-    if (LocalStorage.isLoggedIn()) {  // Використовуємо LocalStorage, а не localStorage
+    if (LocalStorage.isLoggedIn()) {
         const user = LocalStorage.getUser();
         if (userIcon) {
-            userIcon.innerHTML = `<div class="user-icon">${user.name.charAt(0)}</div>`;  // Перша буква імені
+            userIcon.innerHTML = `<div class="user-icon">${user.name.charAt(0)}</div>`;
         }
     } else {
         if (userIcon) {
-            userIcon.innerHTML = `<img src="images/5425129490188725198.jpg" alt="User" id="defaultUserIcon">`; // Стандартна іконка
+            userIcon.innerHTML = `<img src="images/5425129490188725198.jpg" alt="User" id="defaultUserIcon">`;
         }
     }
 
     // Перевірка авторизації та перенаправлення на сторінках
-    if (!LocalStorage.isLoggedIn()) {  // Використовуємо LocalStorage
-        // Перенаправляємо на логін, якщо користувач не авторизований
-        if (currentPage !== '/login.html' && currentPage !== '/register.html') {
+    if (!LocalStorage.isLoggedIn()) {
+        // Перенаправляємо на логін тільки для сторінок, які вимагають авторизації
+        const protectedPages = ['/account.html', '/profile.html']; // Додайте сюди інші захищені сторінки
+        if (protectedPages.includes(currentPage)) {
             window.location.href = 'login.html';
         }
     } else {
         // Якщо користувач залогінений і намагається перейти на login чи register
         if (currentPage === '/login.html' || currentPage === '/register.html') {
-            window.location.href = 'account.html'; // Перенаправляємо на акаунт
+            window.location.href = 'account.html';
         }
     }
 
@@ -33,7 +34,9 @@ window.addEventListener('load', function () {
 
 // Додаємо обробник події на натискання іконки профілю
 document.getElementById('user-icon')?.addEventListener('click', function () {
-    if (!LocalStorage.isLoggedIn()) {  // Використовуємо LocalStorage
-        window.location.href = 'login.html'; // Перенаправляємо на логін, якщо не залогінений
+    if (!LocalStorage.isLoggedIn()) {
+        window.location.href = 'login.html';
+    } else {
+        window.location.href = 'account.html';
     }
 });
